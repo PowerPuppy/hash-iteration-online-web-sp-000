a95abc85ca8d91e49ef65b0d7b696d8eddab9dec
Chris Cheong <chrisxcheong@gmail.com>
2019-07-17 05:48:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,7 @@ birthday_kids = {
 }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
+  if birthday_kids.each do |kids_name, age| age<=12
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
